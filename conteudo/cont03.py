37a1621932f0f58ef68187168decefdd04a8df15
brunofrutuoso-rgb <bruno.frutuoso@escola.pr.gov.br>
2026-03-09 08:55:11 -0300

    Create cont03.py

---
+++ conteudo/cont03.py
@@ -0,0 +1,10 @@
+n = input('digite algo')
+print(n.isnumeric())#para perguntar se é um número
+print(n.isalpha())#para perguntar se é letra
+print(n.isalnum())#para perguntar se é letra ou número
+print(n.isupper())#para perguntar se é so com letra
+print(n.isdecimal())#para verificar se é decimal
+print(n.isdigit())#para verificar se é um digito
+print(n.islower())#para verifar se é apenas letras minusculas
+print(n.isidentifier())#para verificar se é um indentificador válido
+print(n.isspace())#para verifar se tem um espaço em branco
