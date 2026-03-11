20f57f3a1b385c6d6e08e086ab3f6525dd30f59c
brunofrutuoso-rgb <bruno.frutuoso@escola.pr.gov.br>
2026-03-11 11:35:45 -0300

    Create cont04.py

---
+++ conteudo/cont04.py
@@ -0,0 +1,7 @@
+# o + é utilizado como operador de adição
+#o - é utilizado como sinal de subtração
+#O * É UTiLIZADO PARA FAZER A MULTIPLICAÇÃO
+# / é utilizado para faze3r divisão
+#** é utilizado para fazer exponenciação
+#// é utilizado para fazer divisão inteira
+#% é utlizado para fazer o resto da divisao
