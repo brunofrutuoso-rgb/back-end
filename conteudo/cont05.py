c83c8e8b1ebb1a6c8e3e9e70362dee8c9a622d96
brunofrutuoso-rgb <bruno.frutuoso@escola.pr.gov.br>
2026-03-16 10:52:07 -0300

    Create cont05.py

---
+++ conteudo/cont05.py
@@ -0,0 +1,12 @@
+#definição: é a ordem na qual os operadores serão execultados
+#1ºexpressão dentro de parentes
+#3º em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. no caso de ter mais de um deles
+#4º e poir últmo a soma e a subtração
+#ex
+#5+3==
+#3*5+4+
+#3*(5+4)*8=
+#para exponenciação pode ser usado o pow (x.y) onde o número x é elevado ao número y
+#para raiz quadrada podemos usar o **(1/3.) e assim por diante
+#podemos usar os operadores em strings
+# ex'='*5 vai ficar '====='
