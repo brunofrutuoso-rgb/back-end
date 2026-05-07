e29c68ebcdfa209211005da7dc3216c5ced9e4c9
brunofrutuoso-rgb <bruno.frutuoso@escola.pr.gov.br>
2026-05-07 08:14:39 -0300

    Add number analysis functionality in ex23.py

---
+++ ex23.py
@@ -0,0 +1,10 @@
+numero = int(input('informe um número'))
+= numero//1 % 10
+= numero//10 % 10 d
+C = numero//100% 10
+m = numero//1000% 10
+print('Analisando {}'.format(numero))
+print('unidade {}'.format(u))
+print('dezena {}'.format(d))
+print('centana {}'.format(c))
+print('milhar {}'.format(m))
